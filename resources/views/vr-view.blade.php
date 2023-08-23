3078eff28f0ae47f230165abe717dd050e57146a
syy <syya133@gmail.com>
2023-08-23 16:04:35 +0630

    update vr

--- resources/views/vr-view.blade.php
+++ resources/views/vr-view.blade.php
@@ -1,23 +1,19 @@
 @extends('layout.master')
 @section('content')
-    <!-- <model-viewer> HTML element -->
+  <model-viewer src="{{ asset('photo/AAC22.glb') }}" ar ar-modes="scene-viewer webxr quick-look" camera-controls poster="poster.webp" shadow-intensity="1" ios-src="AAC22.usdz" xr-environment>
 
-    <model-viewer src="{{ asset('photo/AAC22.glb') }}" ar ar-modes="scene-viewer webxr quick-look" camera-controls poster="poster.webp" shadow-intensity="1" ios-src="AAC22.usdz" xr-environment>
+    <button slot="ar-button" id="ar-button">
+        View in your space
+    </button>
 
-      <button slot="ar-button" id="ar-button">
-          View in your space
-      </button>
+    <template shadowroot="open">
+        <slot name="ar-button"></slot>
+    </template>
 
-      <template shadowroot="open">
-          <slot name="ar-button"></slot>
-      </template>
-
-      {{-- <button id="ar-button"> Start AR </button> --}}
-      
-      <div id="ar-prompt">
-        <img src="ar_hand_prompt.png">
-      </div>
-    </model-viewer>  
-    <!-- Loads <model-viewer> for browsers: -->
-   
+    {{-- <button id="ar-button"> Start AR </button> --}}
+    
+    <div id="ar-prompt">
+      <img src="ar_hand_prompt.png">
+    </div>
+  </model-viewer> 
 @endsection
